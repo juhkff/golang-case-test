90b7368a9eef60b1c331fa9c818066474e7afaf8
liuyiqing <liuyiqing@xinye.com>
2024-09-30 15:02:10 +0800

    refactor: 优化gin框架的路由处理逻辑
    将全局管道的创建放在router作用域外，以便正确控制并发数。
    同时，增加对每个请求是否单独创建了线程的验证。

--- gin-model-test/web-thread.go
+++ gin-model-test/web-thread.go
@@ -14,12 +14,15 @@ func WebThread() {
 	// Start the web server
 	router := gin.Default()
 	router.Use(gin.Recovery())
-	// 试图创建一个全局管道，用于控制并发数
+	// 实际应该将全局管道放在router作用域外，这样才能控制并发数
 	var limiter = make(chan struct{}, 3)
-	// i的值也可用于验证是否对每个请求单独创建了一个线程
+	// 可以通过i的值验证是否对每个请求单独创建了一个线程
 	var i int64
 	router.GET("/thread", func(c *gin.Context) {
-
+		// 试图创建一个全局管道，用于控制并发数
+		// var limiter = make(chan struct{}, 3)
+		// i的值也可用于验证是否对每个请求单独创建了一个线程
+		// var i int64
 		go func() {
 			index := atomic.AddInt64(&i, 1)
 			limiter <- struct{}{}
@@ -38,7 +41,7 @@ func WebThread() {
 
 func Request() {
 	//发送web请求
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 5; i++ {
 		resp, err := http.Get("http://localhost:8080/thread")
 		if err != nil {
 			fmt.Println("http.Get err:", err)
